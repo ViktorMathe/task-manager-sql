7357490e1cb4248af4714f8aca1bcae8c030c086
ViktorMathe <matheviktor@icloud.com>
2022-10-01 14:52:23 +0000

    Adding task functions

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
 
-  document.addEventListener('DOMContentLoaded', function() {
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
-  });
 
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+  });
