eed74cee0afd5f5d5bb9f277b8b48cd0e050c8be
ViktorMathe <matheviktor@icloud.com>
2022-10-01 13:54:40 +0000

    Fix the modal for the delete category

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+  });
+
